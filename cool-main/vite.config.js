d1e30cbfc7660cce2f4d1e4e053ce2b34ed1d7d8
plop14 <70171493+plero75@users.noreply.github.com>
2025-08-07 02:25:45 +0200

    Add files via upload

--- cool-main/vite.config.js
+++ cool-main/vite.config.js
@@ -1,7 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/cool/',
+  base: '/cool/', // ← Obligatoire pour GitHub Pages à ce chemin
   plugins: [react()]
 })
